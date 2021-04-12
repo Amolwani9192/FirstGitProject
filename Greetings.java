76adcc75ffa2a2c0abbcf3dd714b7478e0112f94
Amol <amolwani9192@gmail.com>
2021-04-12 19:14:29 +0530

    changes in Greeting.java file

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,7 @@
 class Greetings{
 	public static void main(String[] args){
 		System.out.println("Welcome..!!");
+		System.out.println("welcome my friend");
+		System.out.println("if you can dream it - you can do it");
 	}
 }
