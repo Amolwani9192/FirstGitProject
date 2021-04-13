afb58a8c5bb3ed39e715c086b2f1c810968e547c
Amol <amolwani9192@gmail.com>
2021-04-13 13:03:58 +0530

    added rainy day today message

--- Example.java
+++ Example.java
@@ -1,7 +1,7 @@
 public class Example{
 	public static void main(String[] args){
 		System.out.println("Hello world");
-		System.out.println("How are you?");
+		System.out.println("It's a rainy day today");
 		System.out.println("Good bye");
 	}
 }
