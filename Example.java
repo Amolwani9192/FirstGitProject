7614ff8e20bc5eeb64fea9ffa924eef7f05e55f9
Amol <amolwani9192@gmail.com>
2021-04-13 13:02:49 +0530

    changed Example file

--- Example.java
+++ Example.java
@@ -1,7 +1,8 @@
 public class Example{
 	public static void main(String[] args){
 		System.out.println("Hello world");
-		System.out.println("How are you?");
+		System.out.println("It's a good day today");
 		System.out.println("Good bye");
+
 	}
 }
